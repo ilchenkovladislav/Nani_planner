2f7c0f8528976c7d6120e4730a7f0a734e703a8d
Ilchenko <ilchenkowd@gmail.com>
2024-05-16 11:09:19 +0500

    gave the style to the editor

--- src/components/MyEditor/MyEditor.tsx
+++ src/components/MyEditor/MyEditor.tsx
@@ -6,15 +6,30 @@ import Paragraph from "@tiptap/extension-paragraph";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
 import Text from "@tiptap/extension-text";
-import StarterKit from "@tiptap/starter-kit";
+import Bold from "@tiptap/extension-bold";
+import Italic from "@tiptap/extension-italic";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { createPortal } from "react-dom";
+import { animated, useSpring } from "@react-spring/web";
+import { IoCheckboxOutline } from "react-icons/io5";
+import { FiBold } from "react-icons/fi";
+import { FiItalic } from "react-icons/fi";
 
 export function MyEditor() {
     const editor = useEditor({
-        extensions: [Document, Paragraph, Text, StarterKit, TaskList, TaskItem],
+        extensions: [
+            Document,
+            Paragraph,
+            Bold,
+            Italic,
+            Text,
+            TaskList,
+            TaskItem,
+        ],
         content: "",
     });
+
+    const styles = useSpring({ opacity: editor?.isFocused ? 1 : 0 });
 
     if (!editor) {
         return null;
@@ -23,17 +38,12 @@ export function MyEditor() {
     return (
         <>
             {createPortal(
-                <div className="fixed bottom-10 left-0 z-10 flex w-full justify-center gap-2">
-                    <button
-                        onClick={() =>
-                            editor.chain().focus().toggleTaskList().run()
-                        }
-                        className={cn("border p-1 text-center", {
-                            "is-active": editor.isActive("taskList"),
-                        })}
-                    >
-                        Список
-                    </button>
+                <animated.div
+                    style={styles}
+                    className={cn(
+                        "fixed bottom-0 left-0 z-10 flex w-full justify-center gap-2 border-t bg-background",
+                    )}
+                >
                     <button
                         onClick={() =>
                             editor.chain().focus().toggleBold().run()
@@ -41,11 +51,11 @@ export function MyEditor() {
                         disabled={
                             !editor.can().chain().focus().toggleBold().run()
                         }
-                        className={cn("border p-1 text-center", {
+                        className={cn("p-5 text-center text-lg", {
                             "is-active": editor.isActive("bold"),
                         })}
                     >
-                        bold
+                        <FiBold />
                     </button>
                     <button
                         onClick={() =>
@@ -54,13 +64,23 @@ export function MyEditor() {
                         disabled={
                             !editor.can().chain().focus().toggleItalic().run()
                         }
-                        className={cn("border p-1 text-center", {
+                        className={cn("p-5 text-center text-lg", {
                             "is-active": editor.isActive("italic"),
                         })}
                     >
-                        italic
+                        <FiItalic />
                     </button>
-                </div>,
+                    <button
+                        onClick={() =>
+                            editor.chain().focus().toggleTaskList().run()
+                        }
+                        className={cn("p-5 text-center text-xl", {
+                            "is-active": editor.isActive("taskList"),
+                        })}
+                    >
+                        <IoCheckboxOutline />
+                    </button>
+                </animated.div>,
                 document.querySelector("#root")!,
             )}
 
