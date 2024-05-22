aef27283b2ac8b3e116d2f1596eb0ce8b6bc6dc2
Ilchenko <ilchenkowd@gmail.com>
2024-05-22 23:33:33 +0500

    fix date formaters

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,14 +1,14 @@
 import { endOfISOWeek, format, startOfISOWeek } from "date-fns";
 import { ru } from "date-fns/locale";
 
-export const formatDay = (date: Date) => format(date, "d MMMM", { locale: ru });
+export const formatDay = (date: Date) => format(date, "d MMM", { locale: ru });
 
-export const formatMonth = (date: Date) => format(date, "LLLL", { locale: ru });
+export const formatMonth = (date: Date) => format(date, "LLL", { locale: ru });
 
 export function formatWeekRange(date: Date) {
     return `${format(startOfISOWeek(date), "d")} - ${format(
         endOfISOWeek(date),
-        "d MMMM",
+        "d MMM",
         {
             locale: ru,
         },
